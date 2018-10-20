7a8565dbb62a1e6d3f7f4ed694e5eac9c056546b
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-20 12:43:37 -0600

    built map

--- src/mapper/index.js
+++ src/mapper/index.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
 
+import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
+
+import './mapper.css';
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1IjoiaWJkdjQ1MjEiLCJhIjoiY2puaHFxaWQyMGdzajN2cGNza2NxeGdqNyJ9.-9cHdYVwZSFpfy413FUNeQ"
+});
+
 class Mapper extends Component {
   render() {
     return (
       <div>
-        <h2>Here's our mapper</h2>
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: "97vh",
+            width: "100vw",
+          }}
+          center={[
+            -73.9749,
+            40.7736
+          ]}
+        >
+          <Marker
+            coordinates={[
+              -73.9749,
+              40.7736
+            ]}
+            anchor="bottom">
+            {/* <img className='fire-img' src={'https://www.creativefabrica.com/wp-content/uploads/2018/07/Fire-Flame-Logo-Vector-by-DEEMKA-STUDIO-2-580x406.jpg'}/> */}
+            <div className='marker-div'>
+              <p className='marker-text'>Marker</p>
+            </div>
+          </Marker>
+        </Map>
       </div>
     )
   }
 }
 
 export default Mapper;
+
