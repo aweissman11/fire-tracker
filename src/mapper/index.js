c16e9520ecf25b6aa1f3d37d3605853edff0a409
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-20 11:41:14 -0600

    build structure and install google-maps-react

---
+++ src/mapper/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Mapper extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Here's our mapper</h2>
+      </div>
+    )
+  }
+}
+
+export default Mapper;
---
+++ src/reporter/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reporter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am the reporter!!!</h1>
+      </div>
+    )
+  }
+}
+
+export default Reporter;
